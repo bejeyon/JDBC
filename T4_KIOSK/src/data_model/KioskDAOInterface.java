a438c5e0bc179f4a8b96f0a29c0a56f487bd4648
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-11-21 12:26:23 +0900

    Update KioskDAOInterface.java

--- T4_KIOSK/src/data_model/KioskDAOInterface.java
+++ T4_KIOSK/src/data_model/KioskDAOInterface.java
@@ -1,12 +1,6 @@
 package data_model;
 
 public interface KioskDAOInterface {
-	//	void accountInsert(AccountVO newAccount);//계좌추가 추상메서드
-	//	void accountList();//계좌내역 출력 추상메서드
-	//	void accountPlusUpdate(AccountVO account);//계좌잔고 입금 추상메서드
-	//	void accountMinusUpdate(AccountVO account);//계좌잔고 출금 추상메서드
-	//	boolean accountFindOne(String ano);//기존 계좌 유무 check 추상메서드
-	
 	void orderInsert(KioskDTO newKioskOrderDTO);
 	void getMenu(String menu_id, String menu_name, int unit_cost, int unit_price);
 	void setStock(KioskDTO setStockDTO);
